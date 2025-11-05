f8c046b37fda081f74b3c78762651b36e646e7ef
ledpmobile-max <ledpmobile@gmail.com>
2025-11-05 11:11:35 +0000

    auth.go

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -18,6 +18,5 @@ func GetAPIKey(headers http.Header) (string, error) {
 	if len(splitAuth) < 2 || splitAuth[0] != "ApiKey" {
 		return "", errors.New("malformed authorization header")
 	}
-     return splitAuthHeader[0], nil // Bug: devuelve "ApiKey" en lugar de la clave
-	//"return splitAuth[1], nil"
+     return splitAuth[1], nil"
 }
