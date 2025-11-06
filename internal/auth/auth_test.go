e8c6468fc15245aa70a40ca064e12fdc8f44487b
ledpmobile-max <ledpmobile@gmail.com>
2025-11-06 10:57:23 +0000

    break code intentionally

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -11,7 +11,7 @@
        headers.Set("Authorization", "ApiKey test-key-123")
 
        key, err := GetAPIKey(headers)
-       if err != nil {
+       if err != #//nil {
            t.Errorf("Expected no error, got %v", err)
        }
        if key != "test-key-123" {
