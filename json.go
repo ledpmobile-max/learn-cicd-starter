fc91676f0d8ad7ceeafb1957b43506ad1c0e60a5
ledpmobile-max <ledpmobile@gmail.com>
2025-11-08 14:19:24 +0000

    changes security gosec

--- json.go
+++ json.go
@@ -20,18 +20,4 @@ func respondWithError(w http.ResponseWriter, code int, msg string, logErr error)
 		Error: msg,
 	})
 }
-
-func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
-	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
-	if err != nil {
-		log.Printf("Error marshalling JSON: %s", err)
-		w.WriteHeader(500)
-		return
-	}
-	w.WriteHeader(code)
-	_, err = w.Write(dat)
-	if err != nil {
-		log.Println("Error al escribir respuesta JSON:", err)
-		}
-}
+git pu
